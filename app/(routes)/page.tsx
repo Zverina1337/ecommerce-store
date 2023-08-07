be425d77b2b7955cd3fed7adba8aad24937d2f65
zverina1337 <danii.zvorugin@gmail.com>
2023-08-08 06:35:45 +0700

    Added render of categories, products and billboards.

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,19 +1,36 @@
 import Container from "@/components/ui/container";
 import Billboard from "@/components/ui/billboard";
 import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+import ProductList from "@/components/product-list";
 
 const HomePage = async () => {
+    const products = await getProducts({ isFeatured: true })
     const billboard = await getBillboard("ee16b9f1-d07a-4ac4-9f7b-049357de2666")
 
     return (
         <Container>
-           <div className="
-                    space-y-10
-                    pb-10
+            <div className="
+                     space-y-10
+                     pb-10
+                 "
+            >
+                <Billboard data={billboard}/>
+                <div className="
+                    flex
+                    flex-col
+                    gap-y-8
+                    px-4
+                    sm:px-6
+                    lg:px-8
                 "
-           >
-               <Billboard data={billboard}/>
-           </div>
+                >
+                    <ProductList
+                        title="Featured Products"
+                        items={products}
+                    />
+                </div>
+            </div>
         </Container>
     );
 };
